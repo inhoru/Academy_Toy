556440d28b9f9a2c94be24b99687cd6db59c61e9
inhoru <inhoru126@gmail.com>
2023-03-13 08:54:10 +0900

    로또

--- Toy/src/toy/Lotto.java
+++ Toy/src/toy/Lotto.java
@@ -4,7 +4,7 @@ public class Lotto {
 
 	public static void main(String[] args) {
 		// 로또만들기!!
-				// 1~45까지의 중복값없이 6자리로저장하기
+				// 1~45까지의 중복값없이 6자리로저장하기.
 				int[] lotto = new int[6];
 
 				for (int i = 0; i < lotto.length; i++) {
